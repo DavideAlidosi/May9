24bfa97eaed21244187bd518bf96a44ed23cd346
DESKTOP-2VQVR6P\dixit <davide@alidosi.com>
2018-07-25 14:03:59 +0200

    Fix Delete Non Former History script on Locator, Fix deformer apply on NURBS when component are activated by Contextual Hotkey, update documentation

--- may9/plug-ins/MMtoKey/data/data.mm
+++ may9/plug-ins/MMtoKey/data/data.mm
@@ -156,7 +156,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p51
 sg24
 S'CurvesOnSurface_MM'
@@ -212,7 +212,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p62
 sg24
 S'Curves_MM'
@@ -395,7 +395,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p110
 sg24
 S'Nurbs_MM'
@@ -527,7 +527,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p137
 sg24
 S'NurbsCurve_MM'
@@ -546,7 +546,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p141
 sg24
 S'NurbsNurbs_MM'
@@ -678,7 +678,7 @@ I00
 sg21
 I0
 sg22
-S'da_GoToCvsMode'
+S'da_GoToCVsNURBS'
 p168
 sg24
 S'CurveCurve_MM'
